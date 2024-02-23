ef269e273275f076967cbb355ff8c33b5aebbd08
João Augusto <joaoaugustofnc@gmail.com>
2024-02-23 20:59:47 -0300

    Melhorias no app.java

--- src/main/java/br/ufma/ecp/App.java
+++ src/main/java/br/ufma/ecp/App.java
@@ -2,46 +2,47 @@ package br.ufma.ecp;
 
 
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 import br.ufma.ecp.token.Token;
 import br.ufma.ecp.token.TokenType; 
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class App 
 {
-  
+
     public static void main( String[] args )
     {
+        StringBuilder outputBuilder = new StringBuilder();
 
-        String input = """
-            if (x < 0) {
-                // prints the sign
-                let sign = "negative";
+        String fileName = "src/test/resources/ExpressionLessSquare/Main.jack"; // Caminho do arquivo a ser lido
+        String input;
+        try {
+            input = Files.readString(Paths.get(fileName));
+            Scanner scan = new Scanner (input.getBytes());
+            for (Token tk = scan.nextToken(); tk.type != TokenType.EOF; tk = scan.nextToken()) {
+                System.out.println(tk);
+                String tokenString = tk.toString();
+                outputBuilder.append(tokenString).append("\n");
             }
-            """;
-          Scanner scan = new Scanner (input.getBytes());
-          for (Token tk = scan.nextToken(); tk.type != TokenType.EOF; tk = scan.nextToken()) {
-              System.out.println(tk);
-          }
+
+            String xmlString = outputBuilder.toString();
+
+            try {
+                FileWriter writer = new FileWriter("output.xml");
+                writer.write(xmlString);
+                writer.close();
+                System.out.println("output.xml escrito com sucesso.");
+            } catch (IOException e) {
+                System.out.println("Erro ao escrever o arquivo.");
+                e.printStackTrace();
+            }
+        } catch (IOException e) {
+            System.err.println("Erro ao ler o arquivo: " + e.getMessage());
+        }
 }
 
-        /*
-        Parser p = new Parser (input.getBytes());
-        p.parse();
-        */
-
-
-        //Parser p = new Parser (fromFile().getBytes());
-        //p.parse();
-
-        /*
-        String input = "489-85+69";
-        Scanner scan = new Scanner (input.getBytes());
-        System.out.println(scan.nextToken());
-        System.out.println(scan.nextToken());
-        System.out.println(scan.nextToken());
-        System.out.println(scan.nextToken());
-        System.out.println(scan.nextToken());
-        Token tk = new Token(NUMBER, "42");
-        System.out.println(tk);
-        */
-    
 }
